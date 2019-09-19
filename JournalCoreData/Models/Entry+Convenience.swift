3917cf66b0465b71dcdf917807f188eefb2c986d
Percy <percival.ngan@gmail.com>
2019-09-19 18:13:13 -0500

    Added files for up to day2

---
+++ JournalCoreData/Models/Entry+Convenience.swift
@@ -0,0 +1,29 @@
+//
+//  Entry+Convenience.swift
+//  JournalCoreData
+//
+//  Created by Spencer Curtis on 8/12/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Entry {
+    
+    convenience init(title: String,
+                     bodyText: String,
+                     timestamp: Date = Date(),
+                     mood: String,
+                     identifier: String = UUID().uuidString,
+                     context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        
+        self.init(context: context)
+        
+        self.title = title
+        self.bodyText = bodyText
+        self.mood = mood
+        self.timestamp = timestamp
+        self.identifier = identifier
+    }
+}
